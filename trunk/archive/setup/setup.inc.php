76c6eb8baa4df05e49da10219b1d897dea5fb842
ralfbecker <ralfbecker@9f900667-689b-4d53-8d77-f9645e5a22d3>
2013-08-14 14:15:52 +0000

    Simple iframe app for benno

---
+++ trunk/archive/setup/setup.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * eGroupWare - InfoLog
+ *
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <rb@stylite.de>
+ * @package archive
+ * @copyright (c) 2013 by Ralf Becker <rb@stylite.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id$
+ */
+
+$setup_info['archive']['name']      = 'archive';
+$setup_info['archive']['version']   = '0.1';
+$setup_info['archive']['app_order'] = 5;
+$setup_info['archive']['tables']    = array();
+$setup_info['archive']['enable']    = 1;
+
+$setup_info['archive']['author'] =
+$setup_info['archive']['maintainer'] = array(
+	'name'  => 'Ralf Becker',
+	'email' => 'rb@stylite.de'
+);
+$setup_info['archive']['license']  = 'GPL';
