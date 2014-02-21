2448b2e9bf41ea0147608b4f74987c94c5b36575
ralfbecker <ralfbecker@9f900667-689b-4d53-8d77-f9645e5a22d3>
2014-02-21 17:06:21 +0000

    hook to allow apps to add addional domains to CSP frame-src, eg. used by sitemgr to allow access to its sites, also fixed not displayed sidebox menu of old apps

--- trunk/archive/index.php
+++ trunk/archive/index.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * EGroupware - MailArchive
+ * EGroupware - Benno Mail Archive
  *
  * @link http://www.egroupware.org
  * @author Ralf Becker <rb@stylite.de>
  * @package archive
- * @copyright (c) 2013 by Ralf Becker <rb@stylite.de>
+ * @copyright (c) 2013/4 by Ralf Becker <rb@stylite.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -13,25 +13,23 @@
 $GLOBALS['egw_info'] = array(
 	'flags' => array(
 		'currentapp'	=> 'archive',
-		'noheader'		=> True,
-		'nonavbar'		=> True,
+		'noheader'		=> false,
+		'nonavbar'		=> false,
 	)
 );
 include('../header.inc.php');
 
-common::egw_header();
-parse_navbar();
-
-$url = 'https://benno-web.stylite.de/benno/index.php?CA=greeting#';
+// get url from setup.inc.php
+include EGW_SERVER_ROOT.'/archive/setup/setup.inc.php';
 
 // jdots already uses an iframe, so no need to create an other one
 if ($GLOBALS['egw']->framework->template == 'jdots')
 {
-	echo "<script type='text/javascript'>\nwindow.setTimeout(\"location='$url';\",10);\n</script>\n";
+	echo "<script type='text/javascript'>\nwindow.setTimeout(\"location='".$setup_info['archive']['url']."';\",10);\n</script>\n";
 }
 else
 {
 	echo "\n".'<div style="width: 100%; height: 100%; min-width: 800px; height: 600px">';
-	echo "\n\t".'<iframe src="'.$url.'" name="site" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0"><a href="'.$site['site_url'].'">'.$site['site_url'].'</a></iframe>';
+	echo "\n\t".'<iframe src="'.$setup_info['archive']['url'].'" name="site" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0"></iframe>';
 	echo "\n</div>\n";
 }
--- trunk/archive/setup/setup.inc.php
+++ trunk/archive/setup/setup.inc.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * eGroupWare - InfoLog
+ * EGroupware - Benno Mail Archive
  *
  * @link http://www.egroupware.org
  * @author Ralf Becker <rb@stylite.de>
  * @package archive
- * @copyright (c) 2013 by Ralf Becker <rb@stylite.de>
+ * @copyright (c) 2013/4 by Ralf Becker <rb@stylite.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -22,3 +22,7 @@ $setup_info['archive']['maintainer'] = array(
 	'email' => 'rb@stylite.de'
 );
 $setup_info['archive']['license']  = 'GPL';
+$setup_info['archive']['hooks']['csp-frame-src'] = 'archive_hooks::csp_frame_src';
+
+// configure your archive url here:
+$setup_info['archive']['url'] = 'https://benno-web.stylite.de/benno/index.php?CA=greeting#';
