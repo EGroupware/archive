e8beed1dd870da148e98b3d2bc30f167ed65896d
ralfbecker <ralfbecker@9f900667-689b-4d53-8d77-f9645e5a22d3>
2014-03-25 09:29:16 +0000

    automatic login into archive

--- trunk/archive/index.php
+++ trunk/archive/index.php
@@ -22,14 +22,25 @@ include('../header.inc.php');
 // get url from setup.inc.php
 include EGW_SERVER_ROOT.'/archive/setup/setup.inc.php';
 
+list($url) = explode('?', $setup_info['archive']['url']);	// remove ?CA=greeting
+$src = $GLOBALS['egw_info']['server']['webserver_url'].'/archive/js/login.js';
+$attrs = array(
+	'CA' => 'login',
+	'login[username]' => $GLOBALS['egw_info']['user']['account_email'],
+	'login[password]' => $GLOBALS['egw_info']['user']['passwd'],
+);
+
 // jdots already uses an iframe, so no need to create an other one
-if ($GLOBALS['egw']->framework->template == 'jdots')
+if (is_a($GLOBALS['egw']->framework, 'jdots_framework') || $_GET['content'])
 {
-	echo "<script type='text/javascript'>\nwindow.setTimeout(\"location='".$setup_info['archive']['url']."';\",10);\n</script>\n";
+	// login.js creates a form with $attrs and posts it to $url
+	echo '<script type="text/javascript" id="archive_login_js" src="'.$src.'" data-url="'.htmlspecialchars($url).
+		'" data-attrs="'.htmlspecialchars(json_encode($attrs)).'"></script>';
 }
 else
 {
-	echo "\n".'<div style="width: 100%; height: 100%; min-width: 800px; height: 600px">';
-	echo "\n\t".'<iframe src="'.$setup_info['archive']['url'].'" name="site" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0"></iframe>';
+	// et2_container class triggers automatic resize of iframe by idots.js
+	echo "\n".'<div style="width: 100%; height: 100%; min-width: 800px;">';
+	echo "\n\t".'<iframe src="'.$_SERVER['PHP_SELF'].'?content=1" class="et2_container" name="site" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0"></iframe>';
 	echo "\n</div>\n";
 }
