92de4173d64b31b12b235a4ce972cc7aea834267
ralf <rb@egroupware.org>
2023-03-15 20:32:40 +0100

    adapt to work in 23.1 (and before)

--- js/login.js
+++ js/login.js
@@ -4,23 +4,30 @@
  * @link http://www.egroupware.org
  * @author Ralf Becker <rb@egroupware.org>
  * @package archive
- * @copyright (c) 2014-19 by Ralf Becker <rb@egroupware.org>
+ * @copyright (c) 2014-23 by Ralf Becker <rb@egroupware.org>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  */
 
-egw_LAB.wait(function(){
-	jQuery(function(){
-		var action = jQuery('#archive_login_js').attr('data-url');
-		var form = jQuery(document.createElement('form'))
-			.attr({method: 'POST', action: action})
-			.appendTo(jQuery('body'));
-		var attrs = JSON.parse(jQuery('#archive_login_js').attr('data-attrs'));
-		for(var attr in attrs)
+const interval = window.setInterval(() =>
+{
+	if (typeof window.jQuery !== 'undefined')
+	{
+		window.clearInterval(interval);
+
+		jQuery(() =>
 		{
-			jQuery(document.createElement('input'))
-				.attr({type: 'hidden', name: attr, value: attrs[attr]})
-				.appendTo(form);
-		}
-		form[0].submit();
-	});
-});
+			var action = jQuery('#archive_login_js').attr('data-url');
+			var form = jQuery(document.createElement('form'))
+				.attr({method: 'POST', action: action})
+				.appendTo(jQuery('body'));
+			var attrs = JSON.parse(jQuery('#archive_login_js').attr('data-attrs'));
+			for(var attr in attrs)
+			{
+				jQuery(document.createElement('input'))
+					.attr({type: 'hidden', name: attr, value: attrs[attr]})
+					.appendTo(form);
+			}
+			form[0].submit();
+		});
+	}
+}, 200);
